56ea1e8c7f828561e45dacac1ad2d7181a3b3566
Bambooegy <bubbleteacairo@gmail.com>
2025-12-27 13:07:56 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,79 @@
+// menuData القديم
+const menuData = {
+  "Coffee Boba": [
+    ["Iced Latte Boba", 150, 180],
+    ["Dalgona Boba", 170, 195],
+    ["Spanish Latte Boba", 155, 185]
+  ],
+  "Popping Boba": [
+    ["Red Bull Popping Boba", 150, 175],
+    ["Popping Boba Fruit Tea", 130, 160]
+  ],
+  "Milk Tea Boba": [
+    ["Classic Boba", 120, 155],
+    ["Brown Sugar Milk Boba", 135, 165]
+  ],
+  "Taro Boba": [
+    ["Classic Taro", 150, 180],
+    ["Brown Sugar Taro Boba", 160, 185]
+  ],
+  "Matcha Boba": [
+    ["Matcha Boba", 155, 185],
+    ["Brown Sugar Matcha", 165, 185],
+    ["Strawberry Matcha Latte Boba", 175, 190]
+  ],
+  "Milk Boba": [
+    ["Oreo Milk Boba", 160, 195],
+    ["Lotus Milk Boba", 160, 195],
+    ["Chocolate Milk Boba", 155, 190],
+    ["Caramel Milk Boba", 155, 190],
+    ["Red Velvet Milk Boba", 160, 195],
+    ["Mango Milk Boba", 150, 180],
+    ["Blueberry Milk Boba", 150, 180],
+    ["Strawberry Milk Boba", 150, 180],
+    ["Watermelon Milk Boba", 150, 180]
+  ],
+
+  // الأصناف الجديدة التي لم تكن موجودة
+  "Ice-Cream": [
+    ["Mix flavor", 55, 115]
+  ],
+  "Milkshake": [
+    ["Strawberry Milkshake", 140, 165]
+    // Raspberry Milkshake غير منشور
+  ],
+  "Iced Coffee": [
+    ["Iced Latte", 125],
+    ["Iced Spanish Latte", 135],
+    ["Iced Americano", 90]
+  ],
+  "Desserts": [
+    ["Walnut and Caramel Cheesecake", 120],
+    ["Lotus Cheesecake", 120],
+    ["Blueberry Cheesecake", 120]
+  ],
+  "Hot Drinks": [
+    ["Espresso", 60],
+    ["Double Espresso", 85],
+    ["Americano", 75],
+    ["Cappuccino", 95],
+    ["Caffè Latte", 95],
+    ["Spanish Latte", 110],
+    ["Macchiato", 95],
+    ["Cortado", 95],
+    ["French Coffee", 85],
+    ["Flat White", 95],
+    ["Mocha", 95],
+    ["Turkish Coffee", 70],
+    ["Black Tea", 45],
+    ["Green Tea", 55],
+    ["Hot Chocolate", 110]
+  ],
+  "Fresh Juices": [
+    ["Fresh Mango", 85],
+    ["Fresh Strawberry", 85],
+    ["Orange Juice", 85],
+    ["Lemon Mint", 85],
+    ["Banana Milkshake", 85]
+  ]
+};
