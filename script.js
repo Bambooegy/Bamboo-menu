5c1043f23ef7eb1dfd8cb961eef4ee4ca950817a
Bambooegy <bubbleteacairo@gmail.com>
2025-12-31 13:25:39 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 const menuData = {
   "Coffee Boba":[["Iced Latte Boba",150,180,"iced-latte-boba.jpg"],["Dalgona Boba",170,195,"dalgona-boba.jpg"],["Spanish Latte Boba",155,185,"spanish-latte-boba.jpg"]],
   "Popping Boba":[["Red Bull Popping Boba",150,175,"red-bull-popping-boba.jpg"],["Popping Boba Fruit Tea",130,160,"popping-boba-fruit-tea.jpg"]],
-  "Milk Tea Boba":[["Classic Boba",120,155,"classic-milk-tea-boba.jpg"],["Brown Sugar Milk Boba",135,165,"brown-sugar-boba.jpg"]],
+  "Milk Tea Boba":[["Classic Boba",120,155,"classic-boba.jpg"],["Brown Sugar Milk Boba",135,165,"brown-sugar-boba.jpg"]],
   "Taro Boba":[["Classic Taro",150,180,"taro-boba.jpg"],["Brown Sugar Taro Boba",160,185,"brown-sugar-taro-boba.jpg"]],
   "Matcha Boba":[["Matcha Boba",155,185,"matcha-boba.jpg"],["Brown Sugar Matcha",165,185,"brown-sugar-matcha.jpg"],["Strawberry Matcha Latte Boba",175,190,"strawberry-matcha-boba.jpg"]],
   "Milk Boba":[["Oreo Milk Boba",160,195,"oreo-milk-boba.jpg"],["Lotus Milk Boba",160,195,"lotus-milk-boba.jpg"],["Chocolate Milk Boba",155,190,"chocolate-milk-boba.jpg"],["Caramel Milk Boba",155,190,"caramel-milk-boba.jpg"],["Red Velvet Milk Boba",160,195,"red-velvet-milk-boba.jpg"],["Mango Milk Boba",150,180,"mango-milk-boba.jpg"],["Blueberry Milk Boba",150,180,"blueberry-milk-boba.jpg"],["Strawberry Milk Boba",150,180,"strawberry-milk-boba.jpg"],["Watermelon Milk Boba",150,180,"watermelon-milk-boba.jpg"]],
@@ -117,4 +117,5 @@ function sendWhatsApp(){
 
 /* ================= INIT ================= */
 renderMenu();
+
 
